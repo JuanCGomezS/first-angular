8077cbda53f1e399094f53aff679ef591c891386
Juan Carlos Gomez <eslost07@gmail.com>
2023-05-30 16:48:49 -0500

    fusión con backend NodeJs

--- src/app/services/usuario.service.ts
+++ src/app/services/usuario.service.ts
@@ -6,28 +6,38 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class UsuarioService {
-  private myAppUrl = `https://localhost:7148/`;
+  private net = false;
+  private debug = false;
+  private myAppUrl = '';
   private myApiUrl = `api/usuario/`;
 
   constructor(
     private http: HttpClient
   ) {
-
+    if (this.net) {
+      this.myAppUrl = `https://localhost:7148/`;
+    } else {
+      this.myAppUrl = ``;
+    }
   }
 
   getListUsuarios(): Observable<any> {
+    if (this.debug) console.log(`${this.myAppUrl}${this.myApiUrl}`);
     return this.http.get(`${this.myAppUrl}${this.myApiUrl}`)
   }
 
   deleteUser(id: number): Observable<any> {
+    if (this.debug) console.log(`${this.myAppUrl}${this.myApiUrl}${id}`);
     return this.http.delete(`${this.myAppUrl}${this.myApiUrl}${id}`)
   }
 
   saveUser(user: any): Observable<any> {
+    if (this.debug) console.log(`${this.myAppUrl}${this.myApiUrl}`, user);
     return this.http.post(`${this.myAppUrl}${this.myApiUrl}`, user)
   }
 
-  updateUser(id: number, user:any): Observable<any> {
+  updateUser(id: number, user: any): Observable<any> {
+    if (this.debug) console.log(`${this.myAppUrl}${this.myApiUrl}${id}`, user);
     return this.http.put(`${this.myAppUrl}${this.myApiUrl}${id}`, user)
   }
 }
